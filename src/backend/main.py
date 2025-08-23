89c043e70e4be56c68532888b275959c6da271f2
Yuri <jamesavila540@gmail.com>
2025-08-23 15:22:18 +0800

    feat(live_detection)

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
 import io
 import cv2
 from ultralytics import YOLO
@@ -8,10 +9,20 @@ import numpy as np
 app = FastAPI()
 model = YOLO("yolov8n.pt")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["/"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
 @app.get("/")
 def root():
     return {"message": "FastAPI Yolo Working"}
 
+
+# For MIRU img detection
 @app.post("/detect-image")
 async def detect_image(file: UploadFile = File(...)):
     contents = await file.read()
@@ -31,3 +42,34 @@ async def detect_image(file: UploadFile = File(...)):
 
     _, img_encoded = cv2.imencode(".jpg", img);    
     return StreamingResponse(io.BytesIO(img_encoded.tobytes()), media_type="image/jpeg")
+
+
+# For MIRU live detection
+
+def generate_frames():
+    cap = cv2.VideoCapture()
+    while True:
+        success, frame = cap.read()
+        if not success:
+            break
+
+        results = model(frame)
+
+        for r in results:
+            for box in r.boxes:
+                x1, y1, x2, y2 = map(int, box.xyxy[0])
+                cls = model.names[int(box.cls)]
+                conf = float(box.conf)
+                cv2.rectangle(frame, (x1, y1), (x2, y2), [0, 255, 0], 2) 
+                cv2.putText(frame, f"{cls} {conf:.2f}", (x1, y1-10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2) #change color later
+        
+        _, buffer = cv2.imencode(".jpg", frame)
+        frame_bytes = buffer.tobytes()
+
+        yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame_bytes + b"\r\n")
+
+        cap.release()
+
+@app.get("/detect-live")
+async def detect_live():
+    return StreamingResponse(generate_frames(), media_type="multipart/x-mixed-replace; boundary=frame")
